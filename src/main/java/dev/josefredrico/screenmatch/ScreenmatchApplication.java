5a51750603c55380e62d95b29075275f866a4b26
Neto <jfredericon@gmail.com>
2024-01-20 17:35:52 -0300

    Adicionado suporte ao SpringWeb

--- src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
+++ src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
@@ -1,27 +1,13 @@
 package dev.josefredrico.screenmatch;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dev.josefredrico.screenmatch.principal.Principal;
-import dev.josefredrico.screenmatch.repository.SerieRepository;
-
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-	@Autowired
-	private SerieRepository repositorio;
+public class ScreenmatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 
 }
